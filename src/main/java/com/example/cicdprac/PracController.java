4b44326fb068a6bed4372c72428f7a5fae21f85b
Yu_Jehyeop <fbwpguq@gmail.com>
2023-05-18 23:38:01 +0900

    환경변수 추가

--- src/main/java/com/example/cicdprac/PracController.java
+++ src/main/java/com/example/cicdprac/PracController.java
@@ -2,21 +2,25 @@ package com.example.cicdprac;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class PracController {
+
+    @Value("${hi.msg}")
+    public String msg;
+
     @GetMapping("/hello")
     public String hi(){
-        return "helloWord!";
+        return "helloWorld!";
     }
 
-    @PostMapping("/memo")
-    public String ssg(String memo) {
-        return memo;
+    @GetMapping("/hi")
+    public String ssg() {
+        return msg;
     }
 }
